ea7019cba95fe90ba62931dfd136803883f20626
Dominique Caron <dominique.caron@mail.mcgill.ca>
2025-01-22 14:04:23 -0800

    move function to R folder

---
+++ R/runBiomass_core.R
@@ -0,0 +1,62 @@
+runBiomass_core <- function(moduleNameAndBranch, paths, cohortData, species, simEnv) {
+  # get modules if using stand alone module
+  if (!is.null(moduleNameAndBranch)) {
+    getModule(moduleNameAndBranch, modulePath = paths$modulePath, overwrite = TRUE) # will only overwrite if wrong version
+  }
+  speciesNameConvention <- LandR::equivalentNameColumn(species$species, LandR::sppEquivalencies_CA)
+  
+  # initialize all cohorts to age 0, biomass of 1, and simulation time to largest longevity
+  cohortDataForYield <- Copy(cohortData)
+  cohortDataForYield$B <- 1L
+  cohortDataForYield$age <- 0L
+  timesForYield <- list(start = 0, end = max(species$longevity))
+  
+  # pick out the elements of the simList that are relevant for Caching -- not everything is
+  modPath <- file.path(paths$modulePath, "Biomass_core")
+  filesToDigest <- c(dir(file.path(modPath, "R"), full.names = TRUE), file.path(modPath, "Biomass_core.R"))
+  dig1 <- lapply(filesToDigest, function(fn) digest::digest(file = fn))
+  
+  dig <- CacheDigest(list(species, cohortDataForYield, timesForYield, dig1))
+  simOutputs <- expand.grid(objectName = "cohortData",
+                            saveTime = unique(seq(timesForYield$start, timesForYield$end, by = 1)),
+                            eventPriority = 1,
+                            fun = "qs::qsave",
+                            # fun = "assign", # arguments = list(value = ll),
+                            stringsAsFactors = FALSE)
+  modulePath <- paste0(paths$modulePath)
+  io <- inputObjects(module = "Biomass_core", path = modulePath)
+  objectNames <- io$Biomass_core$objectName
+  objectNames <- objectNames[sapply(objectNames, exists, envir = simEnv)]
+  objects <- mget(objectNames, envir = simEnv)
+  objects$cohortData <- cohortDataForYield
+  opts <- options("LandR.assertions" = FALSE)
+  on.exit(options(opts))
+  parameters <- list(
+    .globals = list(
+      "sppEquivCol" = speciesNameConvention),
+    Biomass_core = list(
+      ".plotInitialTime" = timesForYield$start
+      , ".plots" = NULL#c("screen", "png")
+      , ".saveInitialTime" = NULL#times$start
+      , ".useCache" = c(".inputObjects", "init")
+      , ".useParallel" = 1
+      , ".maxMemory" = 30
+      , "vegLeadingProportion" = 0
+      , "calcSummaryBGM" = NULL
+      , "seedingAlgorithm" = "noSeeding"
+    )
+  )
+  paths$outputPath <- file.path(paths$outputPath, "cohortDataYield")
+  
+  # run Biomass_core without dispersal
+  simOutputs <- Cache(simInitAndSpadesClearEnv,
+                      paths = paths,
+                      times = timesForYield,
+                      params = parameters,
+                      modules = "Biomass_core",
+                      outputs = simOutputs,
+                      objects = objects,
+                      debug = 1, omitArgs = c("objects", "times", "debug"), .cacheExtra = dig
+  )
+  list(simOutputs = simOutputs, digest = dig)
+}
