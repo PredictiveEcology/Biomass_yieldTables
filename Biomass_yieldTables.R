d7ab6a87a6d8ae3e1731498770b7655342bb3996
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2025-02-05 12:01:51 -0800

    rm pixelGroupRef output

--- Biomass_yieldTables.R
+++ Biomass_yieldTables.R
@@ -110,6 +110,8 @@ doEvent.Biomass_yieldTables = function(sim, eventTime, eventType) {
                              .cacheExtra = mod$digest$outputHash, as.data.table(sim$yieldOutputs)[saved == TRUE])  # function already exists
       message("Converting to CBM Growth Increment ... This may take several minutes")
       cdObjs <- Cache(generateYieldTables, .cacheExtra = mod$digest$outputHash, cohortDataAll, pixelGroupRef = sim$pixelGroupRef, omitArgs = c("cohortData"))
+      # we don't need pixelGroupRef anymore
+      sim$pixelGroupRef <- NULL
       sim$CBM_AGB <- cdObjs$cds
       sim$CBM_speciesCodes <- cdObjs$cdSpeciesCodes
       rm(cdObjs, cohortDataAll)
